4e8a541023c4b851b6814889443ba7fdd2b3aed7
spentium <spentium@naver.com>
2024-06-21 23:54:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 import time
 
-MODEL_LIST = ["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo", "gpt-4o"]
+MODEL_LIST = ["gpt-4o", "gpt-3.5-turbo", "gpt-4-turbo"]
 assistant_id = "asst_Dlr6YRJen7llwFxT393E5noC"
 
 with st.sidebar:
